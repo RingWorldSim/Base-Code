9e869028a7d547fc2fdcc79573ea33596bf619ad
Kevin Crispie <kevincrispie@gmail.com>
2014-12-06 11:59:33 -0500

    modified massForce2.m
    Changed Force of Gravity to be negative and Spring Force to be positive and wrote comments to reflect changes

--- massForce2.m
+++ massForce2.m
@@ -5,20 +5,20 @@ function Force = massForce2( M0,M1,M2,MStar,P0,P1,P2,...
 G=6.67e-11;
 % Forces connecting points act like springs with rest length 0
 % Spring Force is a restoring force and opposes gravity, and is
-% therefore negative
+% therefore positive
 
-Fspring1=-k1*(P0-P1); 
-Fspring2=-k2*(P0-P2);
-
+Fspring1=k1*(P0-P1); 
+Fspring2=k2*(P0-P2);
 %Gravity calculated based on the two nearest masses and the star,
 %not every ringworld mass
+%Gravity is pointed in the negative direction in this coordinate system
 
 %The force is calculated by subtracting the postiions of the objects that
 %act on the mass from the position of the single ringworld mass
 
-FGrav10=G*M0*M1*(P0-P1)./(norm(P0-P1)).^3;
-FGrav20=G*M0*M2*(P0-P2)./(norm(P0-P1)).^3;
-FGravStar0=G*M0*MStar*(P0-PStar)./(norm(PStar-P0)).^3;
+FGrav10=-G*M0*M1*(P0-P1)./(norm(P0-P1)).^3;
+FGrav20=-G*M0*M2*(P0-P2)./(norm(P0-P1)).^3;
+FGravStar0=-G*M0*MStar*(P0-PStar)./(norm(PStar-P0)).^3;
 
 %Returns the force as a vector
 Force=Fspring1+Fspring2+FGrav10+FGrav20+FGravStar0;
