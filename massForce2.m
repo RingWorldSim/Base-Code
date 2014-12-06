f690e890843982a09131508992e8ee066f9077c1
Kevin Crispie <kevincrispie@gmail.com>
2014-12-06 11:04:42 -0500

    modified massForce.m and created new file
    Changes how spring force and gravitational force is calculated to reflect correct direction of forces. Added comments for clarity.

---
+++ massForce2.m
@@ -0,0 +1,25 @@
+function Force = massForce2( M0,M1,M2,MStar,P0,P1,P2,...
+    PStar,k1,k2 )
+%massForce Calculates forces on single ringworld mass
+%   Forces include spring forces and gravitational forces
+G=6.67e-11;
+% Forces connecting points act like springs with rest length 0
+% Spring Force is a restoring force and opposes gravity, and is
+% therefore negative
+
+Fspring1=-k1*(P0-P1); 
+Fspring2=-k2*(P0-P2);
+
+%Gravity calculated based on the two nearest masses and the star,
+%not every ringworld mass
+
+%The force is calculated by subtracting the postiions of the objects that
+%act on the mass from the position of the single ringworld mass
+
+FGrav10=G*M0*M1*(P0-P1)./(norm(P0-P1)).^3;
+FGrav20=G*M0*M2*(P0-P2)./(norm(P0-P1)).^3;
+FGravStar0=G*M0*MStar*(P0-PStar)./(norm(PStar-P0)).^3;
+
+%Returns the force as a vector
+Force=Fspring1+Fspring2+FGrav10+FGrav20+FGravStar0;
+end
