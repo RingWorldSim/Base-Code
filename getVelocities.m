347b2b8a8d74d1afc2fac9834cc65e263e08da70
Kevin Crispie <kevincrispie@gmail.com>
2014-12-06 22:27:51 -0500

    create function to calculate masses of each mass
    Calculates velocity by normalizing position vectors, flipping the vector (reversing x and y) and multiplying it by a scalar v. This gives the velocity vector for each ringworld mass so each has a total velocity v.

---
+++ getVelocities.m
@@ -0,0 +1,25 @@
+function velocities = getVelocities(positions, v)
+%calculates velocity vector of each ringworld mass given position and desired total
+%velocity
+  
+%positions=positions';
+%positions=positions';
+
+%preallocation
+velocities=zeros(size(positions));
+mags=zeros(max(size(positions)),1);
+
+%find magnitude of each position vector, called mags
+for i=1:max(size(positions))
+    mags(i)=norm(positions(:,i));
+end
+
+%normalize each position vector
+for i=1:length(mags)
+    velocities(:,i)=positions(:,i)/mags(i);
+end
+
+%flips the position vector and mutliplies it by the scalar v to get the
+%velocity vectors
+velocities=v*flip(velocities);
+end
