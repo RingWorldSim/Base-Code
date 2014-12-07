7b1bc0063be866cec266349983c7ca58646952da
Kevin Crispie <kevincrispie@gmail.com>
2014-12-07 12:02:23 -0500

    modified output again
    Changed function to only return acceleration, suppressing the positions output. I made the change since I am writing the code for the main Ringworld differently now. Made positions an input but left original code in but commented out. Left comment to explain. Feel free to change.

--- test_find_acceleration.m
+++ test_find_acceleration.m
@@ -1,7 +1,8 @@
-function [positions,accelerations] = test_find_acceleration()
+function accelerations = test_find_acceleration(positions)
     %creates vector of accelrations and positions
         %can call separately to get positions
-    positions = create_positions(4, 3.671*10^6); 
+    %Commented out positions and replaced it with a function input. Left the code in here to help with ease of testing
+    %positions = create_positions(4, 3.671*10^6); 
     %positions, mass of piece, mass of sun, position of sun, spring
     %constant
     accelerations = find_acceleration (positions, 1, 5.972e24, [0;0], 0); %call to find accelerations
