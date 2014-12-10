c06e0b7923b1b29fbdf7d84a3c877d3d19bfc331
Kevin Crispie <kevincrispie@gmail.com>
2014-12-10 10:38:11 -0500

    changed the value part of the function
    changed the value part of the function, added constant for allowable radius

--- events_func.m
+++ events_func.m
@@ -1,7 +1,8 @@
-function [value,isterminal,direction] = events_func(t, W)
+function [value,isterminal,direction] = events_func(t, Y)
     % stop the integration when the ball gets to the wall.
+    allowableDiff=1e2;              %allowable radius difference
     radiusDiff = 1e2;                  % difference between the radii
-    value = W(1) - radiusDiff;        % equals 0 when maximum allowable radiusDiff is reached 
+    value =  radiusDiff-allowableDiff;        % equals 0 when maximum allowable radiusDiff is reached 
     isterminal = 1;                   %will terminate iteration
     direction = 1;                  %approach from either direction
 end
