3d8d37ebfebd176b36fa9ed9647d98ba0880e3fe
Kevin Crispie <kevincrispie@gmail.com>
2014-12-10 10:43:35 -0500

    underscores, not camel case

--- events_func.m
+++ events_func.m
@@ -1,7 +1,7 @@
 function [value,isterminal,direction] = events_func(t, Y)
     % stop the integration when the ball gets to the wall.
     allowableDiff=1e2;              %allowable radius difference
-    radiusDiff = radiusDifference(Y);    % difference between the radii
+    radiusDiff = radius_difference(Y);    % difference between the radii
     value =  radiusDiff-allowableDiff;     % equals 0 when maximum allowable radiusDiff is reached 
     isterminal = 1;                   %will terminate iteration
     direction = 1;                  %approach from either direction
