4b2511d621b6b560ed6b1da350c5ff7c050bd612
Kevin Crispie <kevincrispie@gmail.com>
2014-12-10 10:42:52 -0500

    added call to radiusDifference.m

--- events_func.m
+++ events_func.m
@@ -1,8 +1,8 @@
 function [value,isterminal,direction] = events_func(t, Y)
     % stop the integration when the ball gets to the wall.
     allowableDiff=1e2;              %allowable radius difference
-    radiusDiff = 1e2;                  % difference between the radii
-    value =  radiusDiff-allowableDiff;        % equals 0 when maximum allowable radiusDiff is reached 
+    radiusDiff = radiusDifference(Y);    % difference between the radii
+    value =  radiusDiff-allowableDiff;     % equals 0 when maximum allowable radiusDiff is reached 
     isterminal = 1;                   %will terminate iteration
     direction = 1;                  %approach from either direction
 end
