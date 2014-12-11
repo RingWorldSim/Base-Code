31dc5742dc385bfc048954572b88d231835f970e
Kevin Crispie <kevincrispie@gmail.com>
2014-12-10 19:16:22 -0500

    new events function

---
+++ events_func2.m
@@ -0,0 +1,8 @@
+function [value,isterminal,direction] = events_func2(t, Y)
+    % stop the integration when the ball gets to the wall.
+    allowableDiff=50;              %allowable radius difference
+    radiusDiff = radius_change(Y);    % difference between the radii
+    value =  radiusDiff-allowableDiff;     % equals 0 when maximum allowable radiusDiff is reached 
+    isterminal = 1;                   %will terminate iteration
+    direction = 0;                  %approach from either direction
+end
