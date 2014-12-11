5469f20e174c901a37666e6d163632456bf14c84
Sean Carter <seancarter26@gmail.com>
2014-12-10 19:27:24 -0500

    Fixed radius_change. Hopefully

--- radius_change.m
+++ radius_change.m
@@ -2,11 +2,11 @@ function res = radius_change(Y)
     %Calculates the average orbital radius of the points
     [positions, velocities] = sortData(Y);
 
-    radii = zeros(length(positions)); %a vector containing all of the distances from the sun
+    radii = 0; %a vector containing all of the distances from the sun
     for i = 1:length(positions)
-        radii(i) = norm(positions(:,i));
+        radii = radii + norm(positions(:,i));
     end
     
-    res = mean(radii);
+    res = mean(radii)/length(positions);
     
 end
