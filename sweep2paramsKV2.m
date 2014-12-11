e9523791d3c0cc53e3faab6a542a5e1996b664c3
Kevin Crispie <kevincrispie@gmail.com>
2014-12-11 09:26:41 -0500

    outputs more reasonable graph now

--- sweep2paramsKV2.m
+++ sweep2paramsKV2.m
@@ -5,23 +5,25 @@ function sweep2paramsKV2()
 % determines values of the two paramters to sweep, in this case k and
 % vinitial
 
-k = linspace(0.001,1000,10);
-vinitial = linspace(50,500000,10);
-
+k = linspace(0,100,20);
+vinitial1 = linspace(20000,29800,10);
+vinitial2 = linspace(29800,29800+9800,10);
+vinitial = [vinitial1,vinitial2];
+%vinitial = linspace(0,40000,15);
 %iterates the k and vinitial values over a for loop
 time2break=zeros(length(k),length(vinitial));
 for i=1:length(k)
     for j=1:length(vinitial)
-        time2break(i)=Ringworld2params2(k(i),vinitial(j));
+        time2break(j,i)=Ringworld2params2(k(i),vinitial(j));
     end
 end
-time2break=time2break/(24*60*60);
-pcolor(k,vinitial,time2break) %pcolor graph
+time2break=time2break/(60*60);
+pcolor(k, vinitial, time2break) %pcolor graph
 xlabel('k value (N/m)')
 ylabel('initial velocity (m/s)')
 title('Life of Ringworld')
 c = colorbar();
-ylabel(c,'Time before breaking (days)','FontSize',12);
+ylabel(c,'Time before breaking (hours)','FontSize',12);
 minmax=[0.001, 1000, 50, 500000];
-axis(minmax)
+%axis(minmax)
 end
